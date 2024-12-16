92b8a1724b9154863ab738d36e1545db80f03681
Gurman <gurman@addvalsolutions.com>
2024-12-16 13:22:53 +0530

    update Communication Card

--- project 7/src/components/ui/CommunicationCard.tsx
+++ project 7/src/components/ui/CommunicationCard.tsx
@@ -13,7 +13,6 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import { DatePickerWithPresets } from "./DatePickerWithRange";
 
 export default function CommunicationCard() {
   const chartData = [
@@ -53,7 +52,6 @@ export default function CommunicationCard() {
     <>
  <div className="space-y-10 flex flex-wrap">
       <h2 className="text-left font-bold w-full">Communications</h2>
-      <div><DatePickerWithPresets /></div>
       <div className="flex gap-4 flex-wrap">
         {/* First Card */}
         <Card className="flex-1 min-w-[400px]">
